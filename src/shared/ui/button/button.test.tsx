4ec0e04c383e3068c737ab585e9a651b4a1d0198
Danielgrdnv <danielgrdnv@gmail.com>
2023-09-07 01:31:43 +0700

    added vitest

---
+++ src/shared/ui/button/button.test.tsx
@@ -0,0 +1,11 @@
+import {describe, expect, it} from "vitest";
+import {render, screen} from '@testing-library/react';
+import {Button} from "./button";
+
+describe('button ui', () => {
+    it('Render', () => {
+        render(<Button>test</Button>);
+        expect(screen.getByText('test')).toBeInTheDocument();
+    });
+
+})
