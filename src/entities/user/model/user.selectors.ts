b86b898a4138dfa88fdda96344b1b5149f85cecc
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-09-14 04:04:24 +0300

    fix: yarn

--- src/entities/user/model/user.selectors.ts
+++ src/entities/user/model/user.selectors.ts
@@ -3,4 +3,3 @@ import { RootState } from "@/app/providers/store-provider/store.types";
 export const getUserAuthError = (state: RootState) => state.userReducer.error;
 export const getLoginError = (state: RootState) => state.userReducer.error;
 export const getUserIsAuth = (state: RootState) => state.userReducer.registered;
-export const getUserToken = (state: RootState) => state.userReducer.accessToken;
