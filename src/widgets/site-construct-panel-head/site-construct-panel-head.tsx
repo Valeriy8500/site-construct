0b86924f79058918564a25f83b223f507b54ad41
Valeriy Brazhnik <valerijbrazhnik@yandex.ru>
2023-09-15 04:28:40 +0300

    feat: добавил onChange для названия сайта

--- src/widgets/site-construct-panel-head/site-construct-panel-head.tsx
+++ src/widgets/site-construct-panel-head/site-construct-panel-head.tsx
@@ -1,8 +1,15 @@
 import { Input } from "@/shared/ui/input";
 import { Label } from "@/shared/ui/label";
 import cls from "./site-construct-panel-head.module.scss";
+import { useState } from "react";
 
 export const SiteConstructPanelHead = () => {
+  const [siteName, setSiteName] = useState<string>('');
+
+  const OnChangeSiteName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSiteName(e.target.value);
+  };
+
   return (
     <div className={cls.panel_head}>
       <div className={cls.panel_head__item}>
@@ -16,6 +23,8 @@ export const SiteConstructPanelHead = () => {
           id="site_name"
           name="name"
           className={cls.panel_head__input}
+          onChange={OnChangeSiteName}
+          value={siteName}
         />
       </div>
       <div className={cls.panel_head__item}>
